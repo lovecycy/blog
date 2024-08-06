393d5a00cc6dc54c737180e071e04d0d636e0a0f
cycy <96785236+lovecycy@users.noreply.github.com>
2024-08-06 23:52:44 +0800

    添加动画组件Bloom

---
+++ src/components/animate/Bloom/index.vue
@@ -0,0 +1,40 @@
+<template>
+    <div :class="STATUS_CODE[status]" id="animate-container">
+        <slot></slot>
+    </div>
+</template>
+
+<script setup>
+const STATUS_CODE = {
+   in: 'in',
+   out: 'out'
+}
+defineProps({
+    status: String
+})
+
+</script>
+
+<style scoped>
+#animate-container{
+    background-color: #fff;
+}
+#animate-container.in{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-left: 5px;
+    height: 42px;
+    width: 42px;
+    border-radius: 50px;
+    background-color: transparent;
+    box-shadow: var(--vp-shadow-1);
+    cursor: pointer;
+    transition: all 1s;
+}
+#animate-container.out{
+    width: 150px;
+    height: 300px;
+
+}
+</style>
